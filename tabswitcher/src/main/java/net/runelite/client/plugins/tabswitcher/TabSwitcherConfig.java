36fe76dc2cc10fbed694790a7836563631ef4e54
magnusrn <magnusn@hotmail.co.uk>
2022-01-08 09:25:28 +0000

    add scheduled logout plugin

--- tabswitcher/src/main/java/net/runelite/client/plugins/tabswitcher/TabSwitcherConfig.java
+++ tabswitcher/src/main/java/net/runelite/client/plugins/tabswitcher/TabSwitcherConfig.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.scheduledlogout;
+package net.runelite.client.plugins.tabswitcher;
 
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
--- tabswitcher/src/main/java/net/runelite/client/plugins/tabswitcher/TabSwitcherPlugin.java
+++ tabswitcher/src/main/java/net/runelite/client/plugins/tabswitcher/TabSwitcherPlugin.java
@@ -1,4 +1,4 @@
-package net.runelite.client.plugins.scheduledlogout;
+package net.runelite.client.plugins.tabswitcher;
 
 import com.google.inject.Inject;
 import com.google.inject.Provides;
